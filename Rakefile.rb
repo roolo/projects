f614cf8a4f19148efd067f98b4425d0be40e11d5
Mailo Svetel <development@rooland.cz>
2013-01-07 18:12:30 +0100

    Preserving shell environment behavior

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,16 +1,21 @@
 require 'find'
 require 'logger'
+require 'open3'
 
 namespace :proj do
   desc "Updates all projects"
   task :update_all do
     Find.find('../..') do |f|
       if f =~ /\.git$/
+        zshin, zshout, zsherr = Open3.popen3('zsh')
         repository = f.gsub /\.git$/, ''
         Logger.new(STDOUT).info('Checking '){repository}
-        Dir.chdir(repository){
-          puts `git up`
-        }
+        zshin.puts 'cd %s'%repository
+        zshin.puts 'git up'
+        zshin.puts 'exit'
+        while (line = zshout.gets)
+          puts line
+        end
       end
     end
   end
